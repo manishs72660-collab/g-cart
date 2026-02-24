8488695f557f553387c2decce42dbec431f08862
manish kumar <manishs72660@gmail.com>
2026-02-24 19:03:56 +0530

    add cart page

---
+++ src/component/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Cartpage(){
+    return(
+        <>
+        {/* <h1>I am a cart page</h1> */}
+        </>
+    )
+}
+export default Cartpage;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Products from "./product";
+import Cartpage from "./Cart";
 function Home(){
     return(
         <>
 <Header/>
 <Products/>
+<Cartpage/>
         </>
     )
 }
