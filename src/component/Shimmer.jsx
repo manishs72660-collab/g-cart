5dfec68127c35616bb47d59a9b4578262cf25f3b
manish kumar <manishs72660@gmail.com>
2026-02-26 07:50:18 +0530

    modify shimmer effect

--- src/component/Shimmer.jsx
+++ src/component/Shimmer.jsx
@@ -1,129 +1,324 @@
 import React from "react";
 
 function Shimmer(){
-    return(
+return(
         <>
         <div className="w-[80%] mx-auto mt-20 flex flex-wrap gap-10 justify-center">
-        <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-        <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-        <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-        <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-        <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
-         <div>
-            <div className="h-50 w-60 bg-gray-300 mb-2">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-            <div className="w-60 h-4 bg-gray-400 mb-1">
-            </div>
-        </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+      <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+       <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+       <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+       <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+       <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+       <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+        <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+       <div className="w-64 bg-white rounded-2xl p-4 shadow-lg">
+      
+      {/* Discount Badge */}
+      <div className="w-24 h-6 rounded-full bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Product Image */}
+      <div className="w-full h-56 rounded-xl bg-gray-200 animate-pulse mb-4"></div>
+
+      {/* Title */}
+      <div className="h-5 bg-gray-200 rounded w-3/4 animate-pulse mb-3"></div>
+      <div className="h-5 bg-gray-200 rounded w-2/3 animate-pulse mb-4"></div>
+
+      {/* Rating */}
+      <div className="h-4 bg-gray-200 rounded w-1/3 animate-pulse mb-4"></div>
+
+      {/* Price + Button Row */}
+      <div className="flex items-center justify-between">
+        <div className="h-6 bg-gray-200 rounded w-1/3 animate-pulse"></div>
+        <div className="h-9 w-20 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
         </div>
         </>
     )
